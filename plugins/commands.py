39471b4e5a3b2101b0fc08b4338d7455050a7943
Adithak99 <95956891+Adithak99@users.noreply.github.com>
2023-10-01 21:28:44 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -43,7 +43,7 @@ async def start(client, message):
     if not await db.is_user_exist(message.from_user.id):
         await db.add_user(message.from_user.id, message.from_user.first_name)
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
-    if len(message.command) != 2:
+    if start(message.command) != 2:
         buttons = [[
                     InlineKeyboardButton('⤬ Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ ⤬', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
                 ],[
